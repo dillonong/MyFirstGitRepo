f3e6c21d918b8d609122f95509b287f5378e5e32
dillonong <dillon.ong@gmail.com>
2014-02-28 16:39:50 -0500

    Added index.html and a js from the index.html

---
+++ ProductAppWebAPI/js/fromIndex.js
@@ -0,0 +1,32 @@
+﻿'use strict'
+
+/* This contents come from the Index.html */
+
+var uri = 'api/products';
+
+$(document).ready(function () {
+    //Send an AJAX request ---- not sure if this is used anymore
+    $.getJSON(uri).done(function (data) {
+            //On success, 'data' will contain a list of products.
+            $.each(data, function (key, item) {
+                //Add a list item for the product
+                $('<li>', { text: formatItem(item) }).appendTo($('#products'));
+            });
+        });
+});
+
+function formatItem(item) {
+    return item.Name + ': $' + item.Price;
+}
+
+/* this gets called from the button click onclick event */
+function find() {
+    var id = $('#prodId').val();
+    $.getJSON(uri + '/' + id)
+        .done(function (data) {
+            $('#product').text(formatItem(data));
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            $('#product').text('Error: ' + err);
+        });
+}
