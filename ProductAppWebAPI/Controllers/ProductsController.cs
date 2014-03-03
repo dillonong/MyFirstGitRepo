a46fce0b3adf883664630511e087a1cf684c9fc0
dillonong <dillon.ong@gmail.com>
2014-03-03 11:52:45 -0500

    Changed the Products Controller to use the repository instead

--- ProductAppWebAPI/Controllers/ProductsController.cs
+++ ProductAppWebAPI/Controllers/ProductsController.cs
@@ -10,25 +10,71 @@ namespace ProductAppWebAPI.Controllers
 {
     public class ProductsController : ApiController
     {
-        Product[] products = new Product[]
-        {
-            new Product { Id = 1, Name="Tomato Soup", Category="Groceries", Price=1.23M },
-            new Product {Id=2, Name="Yoyo", Category="Toys", Price=3.75M },
-            new Product{Id=3, Name="Hammer", Category="Hardware", Price=12.03M}
-        };
+        //Using a Repository now instead
+        //Product[] products = new Product[]
+        //{
+        //    new Product { Id = 1, Name="Tomato Soup", Category="Groceries", Price=1.23M },
+        //    new Product {Id=2, Name="Yoyo", Category="Toys", Price=3.75M },
+        //    new Product{Id=3, Name="Hammer", Category="Hardware", Price=12.03M}
+        //};
+        static readonly IProductRepository repository = new ProductRepository();
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return products;
+            //return products;
+            //Now using the repository
+            return repository.GetAll();
         }
 
-        public IHttpActionResult GetProduct(int id)
+        //The following is replaced with using the repository
+        //public IHttpActionResult GetProduct(int id)
+        //{
+        //    var product = products.FirstOrDefault(x=>x.Id == id);
+        //    if (product == null)
+        //        return NotFound();
+        //    else
+        //        return Ok(product);
+        //}
+
+        public Product GetProduct(int id)
         {
-            var product = products.FirstOrDefault(x=>x.Id == id);
-            if (product == null)
-                return NotFound();
-            else
-                return Ok(product);
+            Product item = repository.Get(id);
+            if (item == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return item;
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll().Where(x => String.Equals(x.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        //ADD 
+        public HttpResponseMessage PostProduct(Product item)
+        {
+            item = repository.Add(item);
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+            string uri = Url.Link("DefaultApi", new { id = item.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        //UPDATE
+        public void PutProduct(int id, Product product)
+        {
+            product.Id = id;
+            if (!repository.Update(product))
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+        }
+
+        //DELETE
+        public void DeleteProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            repository.Remove(id);
         }
     }
 }
